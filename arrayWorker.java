92abd7128a27698362e70eb1f9fa49e801930d5a
Chris Caragianis <chriscaragianis@gmail.com>
2015-12-16 11:09:19 -0500

    initial commit

---
+++ arrayWorker.java
@@ -0,0 +1,14 @@
+class arrayWorker {
+
+  //sort an array in ascending order
+  public int[] sort(int[] arr) {
+
+  }
+
+  //returns the index of target in arr if it exists,
+  // returns -1 otherwise
+  public int[] search(int[] arr, int target) {
+
+  }
+
+}
