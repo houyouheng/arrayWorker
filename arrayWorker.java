6b0aa5086fceb72063b7a46d61827fb30ec3dfb3
houyouheng <houyouheng@hotmail.com>
2015-12-18 11:07:11 -0500

    finished project.

--- arrayWorker.java
+++ arrayWorker.java
@@ -17,11 +17,20 @@ class arrayWorker {
 	}
 	//returns the index of target in arr if it exists,
 	// returns -1 otherwise
-	public static void search(int[] arr, int target) {
+	public static int search(int[] array, int target) {
+		for(int i = 0; i < array.length; i++) {
+			if(target == array[i]){
+				return i;
+			}
+		}
+		return -1;
+
 	}
+
 	public static void main(String[] args) {
 	  	int [] array = {69,2,700,14000,80,19,-138,-4};
 		sort(array);
 		System.out.print(Arrays.toString(array));
+		System.out.print(search(array,80));
 	}
 }
